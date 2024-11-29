c24b03282eabf1102800354bdbe3dd3b819083d2
CoderGUY47 <shkhridoy92@gmail.com>
2024-11-29 19:37:47 +0600

    play button work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// In your main JavaScript file
+// Remove unnecessary code and imports
+
+const script = document.createElement('script');
+script.src = './js/guessScript.js'; // Adjust the path
+document.body.appendChild(script);
+
+// Rest of your code remains the same
+
+
 let allPost = document.querySelector(".allPost");
 let tskname = document.querySelector(".tskname");
 let tsktasks = document.querySelector(".tsktasks");
@@ -6,6 +16,11 @@ let updtBtn = document.querySelector(".updtBtn");
 let error = document.querySelector(".error");
 let arr = [];
 let inStore;
+
+function isNumber(value) 
+{
+    return !isNaN(value) && value.trim() !== "";
+}
 
 postBtn.addEventListener("click",function()
 {
@@ -24,12 +39,11 @@ postBtn.addEventListener("click",function()
     } 
     else 
     {
-        arr.push
-        ({
+        //to add the data which is stored in arr and make object
+        arr.push({
             name: tskname.value,
             tasks: tsktasks.value 
         });
-
         tskname.value = "";
         tsktasks.value = "";
         error.innerHTML = "";
@@ -38,41 +52,38 @@ postBtn.addEventListener("click",function()
     }
 });
 
-
-updtBtn.addEventListener("click",function()
-{
+updtBtn.addEventListener("click",function(){
     arr[inStore].name= tskname.value;
     arr[inStore].tasks= tsktasks.value;
     allPost.innerHTML="";
     display()
     postBtn.style.display="inline-block"
     updtBtn.style.display="none"
-    tname.value=""
-    tasks .value=""
+    tskname.value=""
+    tsktasks .value=""
 })
 
-
-function display() 
-{
+function display(){
     allPost.innerHTML="";
     arr.map(item =>
     {
-        let cardColorClass = "";
-        if (!isNaN(item.name) || !isNaN(item.tasks)) 
+        let editBtnTxt="Edit";
+        let cardColor = "";
+        if(isNumber(item.name))
         {
-            cardColorClass = "bg-num";
-        } 
+            editBtnTxt="Play"
+            cardColor="bg-num"
+        }
         else 
         {
-            cardColorClass = "bg-normal"; 
+            cardColor = "bg-normal"; 
         }
-
         allPost.innerHTML += `
-        <div class="card mt-3 ${cardColorClass}" style="width: 18rem;">
+        <div class="card mt-3 ${cardColor}" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">${item.name}</h5>
               <p class="card-text">${item.tasks}</p>
-              <button href="#" class="btn btn-success edt">Edit</button>
+              <button href="#" class="btn btn-success edt">${editBtnTxt}</button>
               <button href="#" class="btn btn-danger dlt">Delete</button>
             </div>
         </div>
@@ -81,10 +92,8 @@ function display()
 
     let dltBtn=document.querySelectorAll(".dlt") 
     let dltBtnConvert=Array.from(dltBtn)
-    dltBtnConvert.map((item,index)=>
-    {
-        item.addEventListener("click", function()
-        {
+    dltBtnConvert.map((item,index)=>{
+        item.addEventListener("click", function(){
             arr.splice(index,1);
             allPost.innerHTML=""
             display();
@@ -93,23 +102,24 @@ function display()
 
     let edtBtn=document.querySelectorAll(".edt")
     let edtBtnConvert=Array.from(edtBtn)
-    edtBtnConvert.map((item2,index)=>
-    {
-        item2.addEventListener("click",function()
-        {
-            tskname.value=arr[index].name
-            tsktasks.value=arr[index].tasks
-            
-            updtBtn.style.display="inline-block"
-            postBtn.style.display="none"
-            
-            inStore=index
+    edtBtnConvert.map((item2,index)=>{
+        item2.addEventListener("click",function(){
+            if(item2.innerHTML=="Play")
+            {
+                alert(`This button is for Guessgame`);
+
+            }
+            else
+            {
+                tskname.value=arr[index].name
+                tsktasks.value=arr[index].tasks
+                
+                updtBtn.style.display="inline-block"
+                postBtn.style.display="none"
+                
+                inStore=index
+            }
         })
     })
-
 }
-
-
-
-
-
+display()
